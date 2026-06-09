e9dd63c5251d9e12f051bb20dc05964ca1c71dcb
LittleSheep <hi@littlesheep.me>
2026-06-10 02:43:16 +0800

    :heavy_plus_sign: Add the opengraph image generator

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["nuxt-lucide-icons", "@nuxt/eslint", "@pinia/nuxt", "@nuxtjs/i18n"],
+  modules: ["nuxt-lucide-icons", "@nuxt/eslint", "@pinia/nuxt", "@nuxtjs/i18n", "@nuxt/fonts", "nuxt-og-image"],
   i18n: {
     strategy: "no_prefix",
     defaultLocale: "en",
